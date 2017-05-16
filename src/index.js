c39b55c8d0b32be452ff6272d307662ad9e7ef09
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-05-16 11:11:06 +0200

    Before demo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import _ from 'lodash';
+// import {forEach} from 'lodash/forEach'
+
+var data = [1, 2, 3, 4, 5];
+
+_.forEach(data, e => console.log(e));
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "./dist/index.bundle.js"
+  }
+};
